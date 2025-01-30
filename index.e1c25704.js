fc56444acbb561e7c6804707dcf64c871c4c3f26
Yurii-Yevsiukov <151449297+Yurii-Yevsiukov@users.noreply.github.com>
2025-01-30 17:56:52 +0000

    Deploying to gh-pages from @ Yurii-Yevsiukov/ice-cream@f4ccf8f147535b66f1a6571a05f1e3d9e45d5209 🚀

---
+++ index.e1c25704.js
@@ -0,0 +1,2 @@
+var mybutton=document.getElementById("btn-back-to-top");function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"}function backToTop(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()},mybutton.addEventListener("click",backToTop);
+//# sourceMappingURL=index.e1c25704.js.map
